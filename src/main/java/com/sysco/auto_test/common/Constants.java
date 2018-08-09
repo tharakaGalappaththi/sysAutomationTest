cfc69b9ce2af6752fa4d0ddf6e39e6ee84d11de2
tharakaGalappaththi <lgtp89@gmail.com>
2018-08-09 20:50:12 +0530

    qcenter02

--- src/main/java/com/sysco/auto_test/common/Constants.java
+++ src/main/java/com/sysco/auto_test/common/Constants.java
@@ -3,9 +3,9 @@ package com.sysco.auto_test.common;
 public class Constants {
     public static final String TEST_ENV = System.getProperty("test.env", "QA");
 //    public static final String TEST_RELEASE = System.getProperty("test.release", "<release name>");
-    public static final String TEST_RELEASE = System.getProperty("Java Based Web UI Automation", "Build_Tharaka_01");
-    public static final String TEST_PROJECT = System.getProperty("test.project", "<project name>");
-    public static final boolean UPDATE_DASHBOARD = Boolean.parseBoolean(System.getProperty("update.dashboard", "false"));
+    public static final String TEST_RELEASE = System.getProperty("test.release", "build_tharaka_01");
+    public static final String TEST_PROJECT = System.getProperty("test.project", "Java Based Web UI Automation");
+    public static final boolean UPDATE_DASHBOARD = Boolean.parseBoolean(System.getProperty("update.dashboard", "true"));
     public static final boolean RUN_LOCALLY = Boolean.parseBoolean(System.getProperty("run.locally", "true"));
     public static final String APP_URL = System.getProperty("app.url", "https://www.bundabergrum.com.au/");
     public static final String APP_OS = System.getProperty("app.os", "WIN10");
